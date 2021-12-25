22d3a8799566ba22921685b78d30be20a143094f
nirzaf <mfmfazrin1986@gmail.com>
2021-12-25 14:43:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace hex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"0x{int.Parse(args[0]):X}");
+
+            }
+            catch
+            {
+                Console.WriteLine("Invalid argument");
+            }
+        }
+    }
+}
